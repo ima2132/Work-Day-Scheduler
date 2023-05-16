9db06d8a1346289a31660c3fe27d81fc1c84d618
ima2132 <ima2132@columbia.edu>
2023-05-16 19:00:29 -0400

    added time block info; need to work on event listeners now

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,14 @@ $(document).ready(function () {
   // generate the time blocks for standard business hours
   var startTime = 9; // 9 AM 
   var endTime = 17; // 5 PM (17 PM in military standard time)
+
+  for (var hour = startTime; hour <= endTime; hour++) {
+    var timeBlock = $("<div>").attr("id", "hour-" + hour).addClass("row time-block");
+
+    var hourText = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(dayjs(hour, "H").format("hA"));
+    var description = $("<textarea>").addClass("col-8 col-md-10 description");
+    var saveBtn = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").html('<i class="fas fa-save" aria-hidden="true"></i>');
+
+    timeBlock.append(hourText, description, saveBtn);
+    $(".container-fluid .row").append(timeBlock);
+  }
