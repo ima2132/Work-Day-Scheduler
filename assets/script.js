ad38713bdd3f5b1ac8cac498f92134d00db9dda1
ima2132 <ima2132@columbia.edu>
2023-05-16 22:15:21 -0400

    made a lot of changes; added a function to get time/improved if condiitonal statements; declared variables that were not listed before; added new variables; still not sure why it doesn't work; need to make some changes

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,31 @@
-// wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// variables for ids in each function
+var currentDay = $("#currentDay");
+var description = $(".description");
+var saveBtn = $(".saveBtn");
+var timeBlock = $(".time-block");
+var today = dayjs();
+
 
 $(document).ready(function () {
 // displays the current day at the top of the calendar
-  $("#currentDay").text(dayjs().format("dddd, MMMM D"));
+  $("#currentDay").text(today.format('[Today is] dddd, MMMM D, YYYY'));
 
 // generate the time blocks for standard business hours
-  var startTime = 9; // 9 AM 
-  var endTime = 17; // 5 PM (17 PM in military standard time)
+for (var hour = 9; hour <= 17; hour++) {
+  var newTimeBlock = $("<div>").attr("id", "hour-" + hour).addClass("row time-block");
 
-  for (var hour = startTime; hour <= endTime; hour++) {
-    var timeBlock = $("<div>").attr("id", "hour-" + hour).addClass("row time-block");
+  var hourText = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(dayjs(hour, "H").format("hA"));
+  var newDescription = $("<textarea>").addClass("col-8 col-md-10 description");
+  var newSaveButton = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").html('<i class="fas fa-save" aria-hidden="true"></i>');
 
-    var hourText = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(dayjs(hour, "H").format("hA"));
-    var description = $("<textarea>").addClass("col-8 col-md-10 description");
-    var saveBtn = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save").html('<i class="fas fa-save" aria-hidden="true"></i>');
+  // appends the elements to the time block
+  newTimeBlock.append(hourText, newDescription, newSaveButton);
 
-    timeBlock.append(hourText, description, saveBtn);
-    $(".container-fluid .row").append(timeBlock);
-  }
+  // appends the time block to the calendar container
+  $(".calendar").append(newTimeBlock);
+}
 
-// loads saved events from local storage
+ // loads saved events from local storage
    loadEvents();
 
 // event listener for save button clicks
@@ -43,24 +47,44 @@ $(document).ready(function () {
     }
   }
 
+// function to add color to the time blocks based on the current time
+function getTime() {
+  $(".time-block").each(function() {
+    var blockHour = parseInt($(this).attr("id"));
+    var now = new Date();
 
-// get the current time
- function updateTimeBlockColors() {
-  var now = new Date(); 
-  
-// update the time block colors based on the current time
-for (var hour =9; hour <=17; hour++){
-  var timeBlock = $("#hour-"+hour);
- 
-// if the current hour is BEFORE the hour of the time block, then the time block should be in the past
-if (now.getHours() < hour) {
-  timeBlock.addClass("past");
-} else if (now.getHours() == hour) {
-// if the current hour is EQUAL TO the hour of the time block, then the time block should be in the present
-  timeBlock.addClass("present");
-} else {
-// if the current hour is AFTER the hour of the time block, then the time block should be in the future
-  timeBlock.addClass("future");
+    // update the time block colors based on the current time
+
+    // if the current hour is BEFORE the hour of the time block, then the time block should be in the past
+    if (now.getHours() < blockHour) {
+      $(this).removeClass("present");
+      $(this).removeClass("future");
+      $(this).addClass("past");
+    }
+    // if the current hour is EQUAL TO the hour of the time block, then the time block should be in the present 
+    else if (now.getHours() === blockHour) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      // if the current hour is AFTER the hour of the time block, then the time block should be in the future
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
 }
-}
-}
+  // calls the getTime() function as a loop to update the time block colors
+  getTime();
+
+  // set values from local storage to the respective text areas
+  $("#hour-9 .description").val(localStorage.getItem("9"));
+  $("#hour-10 .description").val(localStorage.getItem("10"));
+  $("#hour-11 .description").val(localStorage.getItem("11"));
+  $("#hour-12 .description").val(localStorage.getItem("12"));
+  $("#hour-13 .description").val(localStorage.getItem("13"));
+  $("#hour-14 .description").val(localStorage.getItem("14"));
+  $("#hour-15 .description").val(localStorage.getItem("15"));
+  $("#hour-16 .description").val(localStorage.getItem("16"));
+  $("#hour-17 .description").val(localStorage.getItem("17"));
+});
